e74622eda096b1e582a5835f43cad2113be77ff9
Jellyfish_Yu <jellyfishyu0227@gmail.com>
2024-10-31 00:19:26 +0800

    finished lab3 implementation

--- p3.py
+++ p3.py
@@ -1,11 +1,61 @@
-def recursive_sqrt(a, x):
-    # a is an integer, x is the former guess
-    # your statement follows
-    value = 0.5 * (x + a/x)
-    if abs(value - x) > 0.001:
-        value = recursive_sqrt(a, value)
-    return value # value is next square root guess
+import pickle
 
-if __name__ == '__main__':
+def load_data(file):
+    # file is the name of data file (e.g., \texttt{dept-prof.pydata})
+    # Enter your code here
+    result = {}
+    try:
+        with open(file, 'rb') as f:
+            result = pickle.load(f)
+    except IOError as e:
+        print('unable to open the file with error: "{}"'.format(e.args[-1]))
+    except Exception as e:
+        print(e)
+
+    return result
+
+def query(prof_name):
+    # Enter your code here
+    result = []
+    data = {}
+    data = load_data('dept-prof.pydata')
+    for dept in data:
+        if prof_name in data[dept]:
+            result.append(dept)
+    return [x for x in data if prof_name in data[x]]
+
+def update():
+    # Enter your code here
+    data = {}
+    data = load_data('dept-prof.pydata')
+    data['Computer Science'] += data['Artificial Intelligence']
+    del data['Artificial Intelligence']
+    data['Space Engineering'] = ['Mark', 'Neo', 'Jane']
+    try:
+        with open('dept-prof-updated.pydata', 'wb') as fout:
+            pickle.dump(data, fout)
+    except IOError as e:
+        print('unable to open the file with error: "{}"'.format(e.args[-1]))
+    except Exception as e:
+        print(e)
+
+def get_depts_size():
+    # Enter your code here
+    result = {}
+    result = load_data('dept-prof-updated.pydata')
+    for dept in result:
+        result[dept] = len(result[dept])
+    return result
+
+if __name__ == "__main__":
     # you can test your function by using the following
-    print(recursive_sqrt(2, 1))
+    dept_prof = load_data('dept-prof.pydata')
+    print(dept_prof)
+
+    depts = query('John')
+    print(depts)
+
+    dept_prof = update()
+
+    dept_size = get_depts_size()
+    print(dept_size)
--- test_p1.py
+++ test_p1.py
@@ -1,28 +1,23 @@
 import os
-import sys
-# Ensure the working directory is the same as the directory where the file is located.
 current_directory = os.path.dirname(os.path.abspath(__file__))
 os.chdir(current_directory)
 
-# Exercise 1
-if (os.path.isfile('p1.py')):
+if os.path.isfile('p1.py'):
     try:
-        from p1 import divisible_sublist
-        print('Load p1.py')
-        test1 = [[21, 25, 9, 16, 28], [12, 43, 4, 66, 123, 654]]
-        test2 = [3, 4]
-        test3 = [7, 9]
-        expected = [([21, 9, 28], [21], [25, 16]), ([12, 4], [], [43, 66, 123, 654])]
+        from p1 import non_unique
+        print('Successfully load p1.py')
+        test_case = [['a', 'b', 'c', 'a', 'b', 'd'], [], ['abc', 'def', 'abc', 'xyz', 'def']]
+        expected = [['a', 'b'], [], ['abc', 'def']]
         try:
             print('Testing...')
-            answer = list(map(divisible_sublist, test1, test2, test3))
+            answer = list(map(non_unique, test_case))
             if answer == expected:
-                print("You passed all the tests!")
+                print('You passed all the tests!')
             else:
-                print("Wrong answer, you failed the tests!")
+                print('Wrong answer, you failed some of the tests!')
         except:
-            print('Runtime error when testing divisible_sublist, please check your code!')
+            print('Runtime error when testing unique, please check your code')
     except:
-        print('Cannot load divisible_sublist, please check the function name or syntax.')
+        print('Cannot load non_unique function, please check the function name or syntax')
 else:
-    print('Cannot find p1.py, please put p1.py and this test script in the same folder.')
+    print("Cannot find p1.py, please put p1.py and this test script in the same folder.")
